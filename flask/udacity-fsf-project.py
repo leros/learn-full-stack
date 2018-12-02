ec2833075f7ef99f9b257bea6a86c1a886cfd3ea
leros <zhleiyu@gmail.com>
2018-12-02 16:08:12 -0500

    Add new routes for menu items

--- flask/udacity-fsf-project.py
+++ flask/udacity-fsf-project.py
@@ -30,6 +30,21 @@ def restaurantMenu(restaurant_id):
         return output
     except:
         return "no result found for restaurant {}".format(restaurant_id)
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)

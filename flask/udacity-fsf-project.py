827a9df01e0bf322afda0ea6a32ad55a96f54532
leros <zhleiyu@gmail.com>
2018-12-02 15:25:25 -0500

    Add database to project

--- flask/udacity-fsf-project.py
+++ flask/udacity-fsf-project.py
@@ -1,11 +1,29 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
 
-#@app.route('/')
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello World"
+    restaurant = session.query(Restaurant).first()
+    try:
+        items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+        output = ''
+        for i in items:
+            output += i.name
+            output += '</br>'
+        return output
+    except:
+        return "no results"
 
 if __name__ == '__main__':
     app.debug = True
